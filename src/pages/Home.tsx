19f51ec06451cd4a3ced271a98bf089079d71c4b
joao.zanin <103003839+joaowictorzanin@users.noreply.github.com>
2022-06-28 14:39:25 -0300

    v1.0

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,32 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    }
+
+    setTasks([...tasks, newTask])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = tasks.map(task => ({ ...task }))
+
+    const foundTask = updatedTasks.find(task => task.id === id);
+    if(!foundTask){
+      return
+    }
+    else{
+      foundTask.done = !foundTask.done
+      setTasks(updatedTasks)
+    }
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const updatedTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(updatedTasks)
   }
 
   return (
